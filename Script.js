e124d1639cc9736d777e3ee9c84fbe568ae1cb82
Dj-Dago <jonathan.gonzalez05@uptc.edu.co>
2024-10-07 17:13:41 -0500

    al ruedo

--- Script.js
+++ Script.js
@@ -1,3 +1,12 @@
-document.getElementById('alertButton').addEventListener('click', function() {
+document.getElementById('alertButton').addEventListener('click', function () {
     alert('¡Hola! Has hecho clic en el botón.');
 });
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
